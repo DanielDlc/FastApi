15811a9764d1fdb2a085b3d26655c1ea2e7daf73
DanielDlc <dlc.engcomputacao@gmail.com>
2024-04-09 17:13:27 +0100

    delete

--- famp/secao03/main.py
+++ famp/secao03/main.py
@@ -1,3 +1,8 @@
+from typing import List, Optional
+
+from fastapi.responses import JSONResponse
+from fastapi import Response
+
 from fastapi import FastAPI
 from fastapi import HTTPException
 from fastapi import status
@@ -43,6 +48,25 @@ async def post_curso(curso: Curso):
     cursos[next_id] = curso
     del curso.id
     return curso
+
+@app.put('/cursos/{curso_id}')
+async def put_curso(curso_id: int, curso: Curso):
+    if curso_id in cursos:
+        cursos[curso_id] = curso
+        del curso.id
+        return cursos
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'Não existe um curso com o id {curso_id}')
+    
+@app.delete('/cursos/{curso_id}')
+async def delete_curso(curso_id: int):
+    if curso_id in cursos:
+        del cursos[curso_id]
+        return Response(status_code=status.HTTP_204_NO_CONTENT)
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'Não existe um curso com id{curso_id}')
 
 if __name__ == '__main__':
     import uvicorn
