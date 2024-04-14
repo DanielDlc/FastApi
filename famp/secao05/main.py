d8716b825298ef90bc3da4e2b9c8979d20aa5fd8
DanielDlc <dlc.engcomputacao@gmail.com>
2024-04-14 11:20:53 +0100

    main.py

--- famp/secao05/main.py
+++ famp/secao05/main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
 
 from core.configs import settings
-from api.v1.endpoints import api_router
+from api.v1.api import api_router
 
 
-app: FastAPI = FastAPI(title='Curso API - FastAPI SQL Model')
+app = FastAPI(title='Cursos API - FastAPI SQL Alchemy')
 app.include_router(api_router, prefix=settings.API_V1_STR)
+
 
 
 
@@ -13,5 +14,5 @@ app.include_router(api_router, prefix=settings.API_V1_STR)
 if __name__ == '__main__':
     import uvicorn
 
-    uvicorn.run("main:app", host="0.0.0.0",
-                port=8000, log_level='info', reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000,
+                log_level='info', reload=True)
