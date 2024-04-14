6b1d9528347bbfb5bde094de810ab9aaff3ba11d
DanielDlc <dlc.engcomputacao@gmail.com>
2024-04-14 05:23:31 +0100

    Post curso

--- famp/secao05/api/v1/endpoints/curso.py
+++ famp/secao05/api/v1/endpoints/curso.py
@@ -11,3 +11,27 @@ from sqlmodel import select
 
 from models.curso_model import CursoModel
 from core.deps import get_session
+
+
+# Bypass warning SQLModel select
+from sqlmodel.sql.expression import Select, SelectOfScalar
+
+SelectOfScalar.inherit_cache = True # type: ignore
+Select.inherit_cache = True # type: ignore
+# End Bypass
+
+
+router = APIRouter()
+
+
+# POST curso
+@router.post('/', status_code=status.HTTP_201_CREATED, response_model=CursoModel)
+async def post_curso(curso: CursoModel, db: AsyncSession = Depends(get_session)):
+    novo_curso = CursoModel(titulo=curso.titulo,
+                            aulas=curso.aulas,
+                            horas=curso.horas)
+    db.add(novo_curso)
+    await db.commit()
+
+    return novo_curso
+
