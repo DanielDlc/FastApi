92f98091ffc2d6e74a542c873addd8131dca9cff
DanielDlc <dlc.engcomputacao@gmail.com>
2024-04-15 10:39:31 +0100

    config

--- famp/secao05/core/configs.py
+++ famp/secao05/core/configs.py
@@ -3,7 +3,7 @@ from pydantic import BaseSettings
 
 class Settings(BaseSettings):
     API_V1_STR: str = '/api/v1'
-    DB_URL: str = 'postgresql+asyncpg://danieldlc:miguel@localhost:5432/faculdade'
+    DB_URL: str = 'postgresql+asyncpg://danieldlc:digite_a_senha_aqui@localhost:5432/faculdade'
 
     class Config:
         case_sensitive = True
