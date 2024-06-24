dff26259ee75554a447ce6bb4089b2979c3ed9ea
DanielDlc <dlc.engcomputacao@gmail.com>
2024-06-24 21:42:24 +0100

    test app

--- fast_zero/tests/test_app.py
+++ fast_zero/tests/test_app.py
@@ -1,14 +1,28 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from fast_zero.app import app
-
-
-def test_read_root_deve_retornar_ok_e_ola_mundo():
-    client = TestClient(app)  # Arrange (Organização)
-
+def test_read_root_deve_retornar_ok_e_ola_mundo(client):
     response = client.get('/')  # Act (ação)
 
     assert response.status_code == HTTPStatus.OK  # Assert (ok)
     assert response.json() == {'message': 'Olá, Mundo!'}  # Assert
+
+
+def test_create_user(client):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'testusername',
+            'password': 'password',
+            'email': 'test@test.com',
+        },
+    )
+
+    # retornou o status code correto?
+    assert response.status_code == HTTPStatus.CREATED
+    # Validar UserPublic
+    assert response.json() == {
+        'username': 'testusername',
+        'email': 'test@test.com',
+        'id': 1,
+    }
