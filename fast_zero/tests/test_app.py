544c6776fb03db33f56bbbbdd49d85bf12d55892
DanielDlc <dlc.engcomputacao@gmail.com>
2024-06-26 21:11:29 +0100

    test crud

--- fast_zero/tests/test_app.py
+++ fast_zero/tests/test_app.py
@@ -41,3 +41,26 @@ def test_read_users(client):
             }
         ]
     }
+
+
+def test_update_user(client):
+    response = client.put(
+        '/users/1',
+        json={
+            'password': '123',
+            'username': 'testusername2',
+            'email': 'test@test.com',
+            'id': 1,
+        },
+    )
+    assert response.json() == {
+        'username': 'testusername2',
+        'email': 'test@test.com',
+        'id': 1,
+    }
+
+
+def test_deleter_user(client):
+    response = client.delete('/users/1')
+
+    assert response.json() == {'message': 'User deleted'}
